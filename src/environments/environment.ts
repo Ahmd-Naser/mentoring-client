d2be13c2932e72703500541eaee92c382ada575a
ahmd-naser <ahmdnasr197@gmail.com>
2026-08-27 08:57:38 +0300

    removed slash from end of the apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://mentoring.runasp.net/' // ضع هنا رابط الـ API الخاص بـ .NET
+  apiUrl: 'https://mentoring.runasp.net' // ضع هنا رابط الـ API الخاص بـ .NET
 };
