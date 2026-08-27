12e27c540a97dfc1bbd5f63c7e26fe817b66d183
ahmd-naser <ahmdnasr197@gmail.com>
2026-08-27 08:48:13 +0300

    modified environment apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7149' // ضع هنا رابط الـ API الخاص بـ .NET
+  apiUrl: 'https://mentoring.runasp.net/' // ضع هنا رابط الـ API الخاص بـ .NET
 };
